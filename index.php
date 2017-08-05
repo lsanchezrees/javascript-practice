c9b1859e1d2aebd97a126bf50003f866fec8291a
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-08-05 13:23:09 -0600

    created php, js, and css files and first js function

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<script type="text/javascript" src="js/scripts.js"></script>
+		<title>JavaScript Practice</title>
+	</head>
+	<body>
+		<h1>My javascript file</h1>
+		<button type="button" onclick="firstMessage();">Why don't you click here?</button>
+		<div id="dogMsg"></div>
+
+
+
+	</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Leonora on 8/5/2017.
+ */
+function firstMessage() {
+	document.getElementById("dogMsg").innerHTML ="Dogs rule; Cats drool!";
+}
